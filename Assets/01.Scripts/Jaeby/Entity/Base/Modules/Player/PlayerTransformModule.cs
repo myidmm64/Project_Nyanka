ed4a0dcf4961b49bfe28ddd3a6063d16bd52573b
myidmm64 <72392141+myidmm64@users.noreply.github.com>
2022-11-29 12:01:19 +0900

    1129_마스크 에셋

--- Assets/01.Scripts/Jaeby/Entity/Base/Modules/Player/PlayerTransformModule.cs
+++ Assets/01.Scripts/Jaeby/Entity/Base/Modules/Player/PlayerTransformModule.cs
@@ -1,3 +1,4 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,14 +6,31 @@ using static Define;
 
 public class PlayerTransformModule : BaseTransformModule
 {
+    [SerializeField]
+    private CinemachineSmoothPath _path;
+    [SerializeField]
+    private List<Transform> _lookPoints = new List<Transform>();
+    [SerializeField]
+    private GameObject _handMask = null;
+    [SerializeField]
+    private GameObject _faceMask = null;
+    [SerializeField]
+    private GameObject _transEffectPrefab = null;
+    [SerializeField]
+    private List<GameObject> _otherObject = new List<GameObject>();
+
     public override void ChildTransfomationStart()
     {
-
+        CameraManager.Instance.CartCamSelect(_path, _lookPoints[0], 1f);
+        for(int i = 0; i < _otherObject.Count; i++)
+            _otherObject[i].SetActive(false);
+        _handMask.SetActive(true);
     }
 
     public override void ChildTransfomationEnd()
     {
-
+        _faceMask.SetActive(false);
+        _transEffectPrefab.SetActive(false);
     }
 
     public override IEnumerator ChildTransformCoroutine()
@@ -26,6 +44,11 @@ public class PlayerTransformModule : BaseTransformModule
         _mainModule.animator.Update(0);
         yield return new WaitUntil(() =>
         _mainModule.animator.GetCurrentAnimatorStateInfo(1).IsName("Trans") == false);
+        _faceMask.SetActive(true);
+        _handMask.SetActive(false);
+        for (int i = 0; i < _otherObject.Count; i++)
+            _otherObject[i].SetActive(true);
+        _transEffectPrefab.SetActive(true);
         _transed = true;
         mo.SkillRestart(true);
         ClickManager.Instance.ClickModeSet(LeftClickMode.JustCell, false);
