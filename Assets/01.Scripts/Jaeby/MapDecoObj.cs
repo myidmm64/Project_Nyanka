119526bb7ac76228be0b80c939f4fdbefda77b4f
myidmm64 <72392141+myidmm64@users.noreply.github.com>
2022-12-03 18:16:44 +0900

    1203_맵을 가리는 못생긴 거 제거

--- Assets/01.Scripts/Jaeby/MapDecoObj.cs
+++ Assets/01.Scripts/Jaeby/MapDecoObj.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using static Define;
 
 public class MapDecoObj : MonoBehaviour
 {
     private MeshRenderer _meshRenderer = null;
+    private Collider _collider = null;
     private Sequence _seq = null;
     private float _startAlpha = 1f;
     [SerializeField]
@@ -15,8 +17,11 @@ public class MapDecoObj : MonoBehaviour
     private Color _originColor = Color.white;
     private Color _endColor = Color.white;
 
+    private bool _isVisible = true;
+
     private void Start()
     {
+        _collider = GetComponent<Collider>();
         _meshRenderer = GetComponent<MeshRenderer>();
         _originColor = _meshRenderer.material.color;
         _endColor = _originColor;
@@ -25,16 +30,26 @@ public class MapDecoObj : MonoBehaviour
 
     private void OnMouseEnter()
     {
-        Debug.Log("¾Æ¾Ó");
+        if (_isVisible == false) return;
+
+        _isVisible = false;
+        _collider.enabled = false;
         if (_seq != null)
             _seq.Kill();
         _seq = DOTween.Sequence();
-        _meshRenderer.material.color = _originColor;
+        //_meshRenderer.material.color = _originColor;
         _seq.Append(_meshRenderer.material.DOFade(_endAlpha, _duration));
+        _seq.AppendInterval(2f);
+        _seq.AppendCallback(() =>
+        {
+            VisibleAnimation();
+        });
     }
 
-    private void OnMouseExit()
+    private void VisibleAnimation()
     {
+        _collider.enabled = true;
+        _isVisible = true;
         if (_seq != null)
             _seq.Kill();
         _seq = DOTween.Sequence();
