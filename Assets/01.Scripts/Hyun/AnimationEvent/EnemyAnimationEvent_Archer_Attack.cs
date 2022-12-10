4a3662dacfe98910b20992f5a792301ae47c71ed
HyunjunDev <101493216+HyunjunDev@users.noreply.github.com>
2022-12-10 15:49:29 +0900

    Archer AI 완성

--- Assets/01.Scripts/Hyun/AnimationEvent/EnemyAnimationEvent_Archer_Attack.cs
+++ Assets/01.Scripts/Hyun/AnimationEvent/EnemyAnimationEvent_Archer_Attack.cs
@@ -6,8 +6,13 @@ public class EnemyAnimationEvent_Archer_Attack : EnemyAnimationEvent
 {
     private AIMainModule _aIMainModule;
 
+    public float _arrowSpeed;
+
     [SerializeField]
     private GameObject _attackPrefab0 = null;
+
+    [SerializeField]
+    private GameObject hitEffect = null;
 
     private void Start()
     {
@@ -17,25 +22,15 @@ public class EnemyAnimationEvent_Archer_Attack : EnemyAnimationEvent
     public override void AttackAnimation(int id)
     {
         Debug.Log(_aIMainModule.CurrentDir + " !");
-        List<Vector3Int> attackRange = CellUtility.GetAttackVectorByDirections(_aIMainModule.CurrentDir, _aIMainModule.DataSO.normalAttackRange);
-        List<PlayerMainModule> players = CellUtility.FindTarget<PlayerMainModule>(_aIMainModule.ChangeableCellIndex, attackRange, true);
-        //Debug.Log(players.Count);
-        float m_dis = 1000000;
-        PlayerMainModule target = null;
-        foreach (var player in players)
-        {
-            float dis = Vector3Int.Distance(_aIMainModule.ChangeableCellIndex, player.CellIndex);
-            //Debug.Log(dis + " " + player.name);
-            if(m_dis > dis)
-            {
-                m_dis = dis;
-                target = player;
-            }
-        }
-        GameObject obj = Instantiate(_attackPrefab0, target.transform);
-        Destroy(obj, 1.5f);
-        int dmg = Random.Range(_aIMainModule.MinDamage, _aIMainModule.MaxDamage);
-        target.ApplyDamage(dmg, _aIMainModule.elementType, true, false);
+
+        GameObject obj = Instantiate(_attackPrefab0, _aIMainModule.ModelController);
+        obj.transform.SetParent(null);
+        Arrow arrow = obj.AddComponent<Arrow>();
+        arrow.ArrowInit(-_arrowSpeed, transform.position + Vector3.up, Quaternion.LookRotation(-transform.forward), new List<Vector3Int>(),
+            0, 10f, Random.Range(_aIMainModule.MinDamage, _aIMainModule.MaxDamage), _aIMainModule.elementType, Random.Range(0, 100) < 50, false, hitEffect);
+        
+        //int dmg = Random.Range(_aIMainModule.MinDamage, _aIMainModule.MaxDamage);
+        //target.ApplyDamage(dmg, _aIMainModule.elementType, true, false);
     }
 
     public override void AttackEnd()
@@ -47,6 +42,22 @@ public class EnemyAnimationEvent_Archer_Attack : EnemyAnimationEvent
 
     public override void AttackStarted()
     {
-
+        List<Vector3Int> attackRange = CellUtility.GetAttackVectorByDirections(_aIMainModule.CurrentDir, _aIMainModule.DataSO.normalAttackRange);
+        List<PlayerMainModule> players = CellUtility.FindTarget<PlayerMainModule>(_aIMainModule.ChangeableCellIndex, attackRange, true);
+        //Debug.Log(players.Count);
+        float m_dis = 1000000;
+        PlayerMainModule target = null;
+        foreach (var player in players)
+        {
+            float dis = Vector3Int.Distance(_aIMainModule.ChangeableCellIndex, player.CellIndex);
+            //Debug.Log(dis + " " + player.name);
+            if (m_dis > dis)
+            {
+                m_dis = dis;
+                target = player;
+            }
+        }
+        transform.LookAt(target.transform);
+        Debug.Log("?");
     }
 }
--- Assets/01.Scripts/Jaeby/Entity/Player/Suzuka/Arrow.cs
+++ Assets/01.Scripts/Jaeby/Entity/Player/Suzuka/Arrow.cs
@@ -12,9 +12,10 @@ public class Arrow : MonoBehaviour
     int _penetrateCount = 0;
     List<Vector3Int> _boomVectors = new List<Vector3Int>();
     EntityType _entityType = EntityType.None;
+    GameObject _atkObj = null;
     float _speed = 0f;
 
-    public void ArrowInit(float speed, Vector3 position, Quaternion rot, List<Vector3Int> boomVectors, int penetrateCount, float lifeTime, int dmg, ElementType type, bool critical, bool isPlayer)
+    public void ArrowInit(float speed, Vector3 position, Quaternion rot, List<Vector3Int> boomVectors, int penetrateCount, float lifeTime, int dmg, ElementType type, bool critical, bool isPlayer,GameObject obj = null)
     {
         _speed = speed;
         transform.position = position;
@@ -26,6 +27,7 @@ public class Arrow : MonoBehaviour
         _isPlayer = isPlayer;
         _entityType = _isPlayer ? EntityType.Player : EntityType.Enemy;
         _boomVectors = boomVectors;
+        _atkObj = obj;
 
         Destroy(gameObject, lifeTime);
     }
@@ -36,11 +38,14 @@ public class Arrow : MonoBehaviour
         if (mainModule == null) return;
         if (mainModule.entityType == _entityType) return;
         mainModule.ApplyDamage(_dmg, _type, _critical, _isPlayer);
+        GameObject o = Instantiate(_atkObj, mainModule.CellIndex, Quaternion.identity);
+        Destroy(o, 2f);
         List<Cell> cells = CellUtility.SearchCells(mainModule.CellIndex, _boomVectors, true);
         for (int i = 0; i < cells.Count; i++)
             cells[i].CellAttack(_dmg, _type, _entityType);
 
         _penetrateCount--;
+        
         if (_penetrateCount <= 0)
             Destroy(gameObject);
     }
