ffa0f845ab28df35300e06a182bf610ff616bee6
myidmm64 <72392141+myidmm64@users.noreply.github.com>
2022-12-18 10:10:03 +0900

    1218_스타트씬 타이트ㄹ빼고

--- Assets/01.Scripts/Jaeby/StartScene/StartSceneManager.cs
+++ Assets/01.Scripts/Jaeby/StartScene/StartSceneManager.cs
@@ -1,18 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using UnityEngine.SceneManagement;
 
 public class StartSceneManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Sequence _seq = null;
+    private RectTransform _prevTrm = null;
+
+    public void ButtonAnimationStart(RectTransform trm)
     {
-        
+        trm.DOKill();
+        trm.DOAnchorPosX(-100f, 0.2f);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ButtonAnimationEnd(RectTransform trm)
     {
-        
+        trm.DOKill();
+        trm.DOAnchorPosX(-200f, 0.2f);
+    }
+
+    public void GoTutorial()
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
+
+    public void GoGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void GoExit()
+    {
+        Application.Quit();
+    }
+
+    public void GoCredit()
+    {
+
     }
 }
